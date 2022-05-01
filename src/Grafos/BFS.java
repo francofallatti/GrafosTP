f591e1ecb6dae256b74c434a25d3f91c0da9ab01
francofallatti <66625402+francofallatti@users.noreply.github.com>
2022-04-30 21:19:40 -0300

    BFS + BFSTest

---
+++ src/Grafos/BFS.java
@@ -0,0 +1,55 @@
+package Grafos;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BFS {
+	
+	private static List<Integer>L;
+	private static boolean marcados[];
+	
+	public static boolean esConexo(Grafo g) {
+		if(g == null) {
+			throw new IllegalArgumentException("Grafo null");
+		}
+		if (g.tamano()==0) {
+			return true;
+		}
+		return alcanzables(g,0).size()==g.tamano();
+		
+	}
+
+	public static Set<Integer> alcanzables(Grafo g, int origen) {
+		Set<Integer> ret = new HashSet<Integer>();
+		
+		//inicializar
+		L = new ArrayList<Integer>();
+		L.add(origen);
+		marcados = new boolean[g.tamano()];
+		
+		while(L.size()>0) {
+			int i= L.get(0);//Seleccionar i∈ L y marcarlo;
+			marcados[i] = true;
+			ret.add(i);
+			
+			//Agregar a L todos los vecinos no marcados de i;			
+			agregarVecinosPendientes(g, i);
+			
+			L.remove(0);//L := L\{i};;
+		}
+		
+		return ret;
+	}
+
+	private static void agregarVecinosPendientes(Grafo g, int i) {
+		
+		for(int vertices : g.vecinos(i)) {
+			if(marcados[vertices]==false && L.contains(vertices)==false) {
+				L.add(vertices);
+			}
+		}
+		
+	}
+}
---
+++ src/Grafos/GrafoConPeso.java
@@ -0,0 +1,10 @@
+package Grafos;
+
+public class GrafoConPeso extends Grafo{
+
+	public GrafoConPeso(int n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+	}
+
+}
