5f97b50c77eec54e585452c5d52406c2a5e64b00
Candela Gonzalez <gonzalezcandela759@gmail.com>
2022-06-15 21:53:44 -0300

    limpiando el codigo

--- src/Grafos/GrafoConPeso.java
+++ src/Grafos/GrafoConPeso.java
@@ -6,17 +6,10 @@ import java.util.Set;
 
 public class GrafoConPeso extends Grafo {
 	private double[][] matrizConPesos;
-	// private List<Double> pesosDeAristas;
-	// private List<Tupla<Integer, Integer>> aristas;
-	// private Double minActual;
 
 	public GrafoConPeso(int n) {
 		super(n);
 		matrizConPesos = new double[n][n];
-		/*
-		 * aristas = new LinkedList<Tupla<Integer,Integer>>(); pesosDeAristas = new
-		 * LinkedList<Double>(); minActual = Double.MAX_VALUE;
-		 */
 	}
 
 	public void agregarPesoArista(double peso, int i, int j) {
